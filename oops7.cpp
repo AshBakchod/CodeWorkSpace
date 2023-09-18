4c19fd9fa0fa4eb8437114c85073b106e3b102ea
Harshal <114017724+AshBakchod@users.noreply.github.com>
2023-09-18 23:18:44 +0530

    Update oops7.cpp

--- oops7.cpp
+++ oops7.cpp
@@ -1,30 +1,30 @@
+// Online C++ compiler to run C++ program online
 #include <iostream>
 using namespace std;
-class MyClass {
-private:
-    static int cre;
-
-public:
-    MyClass() {
-        cre++;
+class Object{
+    int data;
+    static int cre,des;
+    public:
+    Object(){
+         cre++; cout<<"the number of Objects created : "<<cre<<endl;
     }
-
-    ~MyClass() {
-        cre--;
+    Object(int a){
+        data=a;
+        cre++; cout<<"the number of Objects created : "<<cre<<endl;
     }
-
-    static int getCre() {
-        return cre;
+    ~Object(){
+        des++;
+        cout<<"the number of Objects destroyed : "<<des<<endl;
+    }
+    void show(){
+        cout<<"The Data Value is : "<<data<<endl;
     }
 };
-
-int MyClass::cre = 0;
-
+int Object::cre;
+int Object::des;
 int main() {
-    MyClass obj1;
-    MyClass obj2;
-    MyClass obj3;
-
-    cout<<"Number of objects created: "<<MyClass::getCre()<<endl;
+    // Write C++ code here
+    Object c1,c2(4),c3;
+    c2.show();
     return 0;
 }
