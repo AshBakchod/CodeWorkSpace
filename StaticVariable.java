a54a77f6bef007bff301d1d2ca452c19ea6de7af
Harshal <114017724+justharshal2023@users.noreply.github.com>
2024-02-29 22:15:23 +0530

    Update StaticVariable.java

--- StaticVariable.java
+++ StaticVariable.java
@@ -5,7 +5,7 @@ class Student {
     static String college;
     void setStudent(Scanner sc){
         System.out.print("Enter Student name : ");
-        name = sc.nextLine();
+        name = sc.next();
         System.out.print("Enter Student's rollNumber : ");
         roll = sc.nextInt();
     }
@@ -14,10 +14,16 @@ class Student {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter College name : ");
+        System.out.print("Enter College name for all Students : ");
         Student.college = sc.nextLine();
-        Student s = new Student();
-        s.setStudent(sc);
-        s.display();
+        Student s1 = new Student();
+        s1.setStudent(sc);
+        s1.display();
+        Student s2 = new Student();
+        s2.setStudent(sc);
+        s2.display();
+        Student s3 = new Student();
+        s3.setStudent(sc);
+        s3.display();
     }
 }
